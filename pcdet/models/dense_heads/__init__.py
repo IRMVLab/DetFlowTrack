91e40c58a92d768bfbf5496e9b10caeed5d571eb
shenyueling56 <3247253259@qq.com>
2022-03-12 20:19:48 +0800

    去掉head中对SSD的依赖

--- pcdet/models/dense_heads/__init__.py
+++ pcdet/models/dense_heads/__init__.py
@@ -4,8 +4,8 @@ from .anchor_head_template import AnchorHeadTemplate
 from .point_head_box import PointHeadBox
 from .point_head_simple import PointHeadSimple
 from .point_intra_part_head import PointIntraPartOffsetHead
-from .point_head_box_3DSSD import PointHeadBox3DSSD
-from .point_multi_head_box_3DSSD import PointHeadMultiScaleBox3DSSD
+# from .point_head_box_3DSSD import PointHeadBox3DSSD
+# from .point_multi_head_box_3DSSD import PointHeadMultiScaleBox3DSSD
 
 __all__ = {
     'AnchorHeadTemplate': AnchorHeadTemplate,
@@ -14,6 +14,6 @@ __all__ = {
     'PointHeadSimple': PointHeadSimple,
     'PointHeadBox': PointHeadBox,
     'AnchorHeadMulti': AnchorHeadMulti,
-    'PointHeadBox3DSSD': PointHeadBox3DSSD,
-    'PointHeadMultiScaleBox3DSSD': PointHeadMultiScaleBox3DSSD,
+    # 'PointHeadBox3DSSD': PointHeadBox3DSSD,
+    # 'PointHeadMultiScaleBox3DSSD': PointHeadMultiScaleBox3DSSD,
 }
